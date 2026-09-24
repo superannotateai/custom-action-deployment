a364b43a68eb5de3e3b28136160734551957a698
ArmanSuperannotate <Arman@superannotate.com>
2026-09-24 11:35:10 +0400

    Send x-api-key when SA_TOKEN is not an SDK token.

--- pipe/pipe.js
+++ pipe/pipe.js
@@ -148,6 +148,25 @@ function sanitizeToken(token) {
   return cleanToken;
 }
 
+const TOKEN_PATTERN = /^[-.@_A-Za-z0-9]+=\d+$/;
+
+function isSdkToken(token) {
+  return TOKEN_PATTERN.test(token);
+}
+
+function getAuthHeaders(apiToken) {
+  console.log(isSdkToken(apiToken, 333333333333333));
+  if (isSdkToken(apiToken)) {
+    return {
+      Authorization: apiToken,
+      "Auth-Type": "sdk",
+    };
+  }
+  return {
+    "x-api-key": apiToken,
+  };
+}
+
 /**
  * Get changed files in a specific folder
  */
@@ -405,8 +424,7 @@ async function checkTaskExists(name, apiToken) {
     const response = await makeRequest(url.toString(), {
       method: "GET",
       headers: {
-        Authorization: apiToken,
-        "Auth-Type": "sdk",
+        ...getAuthHeaders(apiToken),
         Referer: "https://app.superannotate.com/",
         "Content-Type": "application/json",
         "User-Agent": `Github Pipeline: ${VERSION}`,
@@ -479,8 +497,7 @@ async function syncTask(folder, apiToken) {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            Authorization: apiToken,
-            "Auth-Type": "sdk",
+            ...getAuthHeaders(apiToken),
             Referer: "https://app.superannotate.com/",
             "User-Agent": `Github Pipeline: ${VERSION}`,
           },
@@ -524,8 +541,7 @@ async function syncTask(folder, apiToken) {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
-            Authorization: apiToken,
-            "Auth-Type": "sdk",
+            ...getAuthHeaders(apiToken),
             Referer: "https://app.superannotate.com/",
             "User-Agent": `Github Pipeline: ${VERSION}`,
           },
@@ -580,6 +596,8 @@ if (require.main === module) {
 
 module.exports = {
   sanitizeToken,
+  isSdkToken,
+  getAuthHeaders,
   getChangedFilesInFolder,
   getChangedFolders,
   generatePayload,
